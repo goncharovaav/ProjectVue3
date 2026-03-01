26a82c1679674c3fd681a41ca19ad42b4bbbb4cc
Anastasia <goncharovaanastasia53@gmail.com>
2026-03-02 00:07:52 +0700

    add base layout styles

--- js/main.js
+++ js/main.js
@@ -8,6 +8,18 @@ Vue.component('kanban-board', {
     `
 });
 
+Vue.component('kanban-column', {
+    props: ['title', 'tasks', 'index'],
+    template: `
+        <div class="column">
+            <h2>{{ title }}</h2>
+            <button v-if="index === 0" class="add-task-btn" @click="$emit('add-click')">
+                Добавить задачу
+            </button>
+        </div>
+    `
+});
+
 new Vue({
     el: '#app',
     template: '<kanban-board></kanban-board>'
