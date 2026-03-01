5552a4e56271c2bb440ee9aa7c22d4e1b1d56898
Anastasia <goncharovaanastasia53@gmail.com>
2026-03-01 23:36:52 +0700

    The basic structure of the project

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-Vue.component('board', {
+Vue.component('kanban-board', {
+    template: `
+        <div>
+            <h1>Kanban Board</h1>
+            <div class="board">
+            </div>
+        </div>
+    `
+});
 
-})
-
-let app = new Vue({
+new Vue({
     el: '#app',
-})
+    template: '<kanban-board></kanban-board>'
+});
